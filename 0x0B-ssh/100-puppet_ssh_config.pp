1909cdc5e96c658a60ce8562a369d8c0bd6f4b26
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-03-24 19:36:55 +0100

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,12 @@
 # Puppet to set changes in a server
+
 file_line { 'disable password login':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
+
 file_line { 'add path to find the keys':
   path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
+  line => 'IdentityFile ~/.ssh/school',
 }
+
